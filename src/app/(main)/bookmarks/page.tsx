69f3df210b30a301a1e3f80cf969ae76d5d5a98b
SysWhiteDev <syswhite.commercial@gmail.com>
2024-08-28 16:57:28 +0200

    finished bookmarks feature

--- src/app/(main)/bookmarks/page.tsx
+++ src/app/(main)/bookmarks/page.tsx
@@ -1,8 +1,80 @@
+"use client";
+
+import InfiniteScrollContainer from "@/components/InfiniteScrollContainer";
+import Post from "@/components/posts/Post";
+import PostsLoadingSkeleton from "@/components/posts/PostsLoadingSkeleton";
+import kyInstance from "@/lib/ky";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { Loader2 } from "lucide-react";
+
 export default function Page() {
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery({
+    queryKey: ["post-feed", "bookmarks"],
+    queryFn: ({ pageParam }) =>
+      kyInstance
+        .get(
+          "/api/bookmarks",
+          pageParam ? { searchParams: { cursor: pageParam } } : {},
+        )
+        .json<any>(),
+    initialPageParam: null as string | null,
+    getNextPageParam: (lastPage) => lastPage.nextCursor,
+  });
+
+  const posts = data?.pages.flatMap((page) => page.posts) || [];
+
+  if (status === "pending") {
+    return (
+      <div className="w-full">
+        <PostsLoadingSkeleton />
+      </div>
+    );
+  }
+
+  if (status === "error") {
+    return (
+      <p className="mx-auto w-full text-center text-destructive">
+        An error occurred while trying to load your bookmarks
+      </p>
+    );
+  }
+
+  if (status === "success" && !posts.length && !hasNextPage) {
+    return (
+      <div className="w-full">
+        <div className="flex flex-col items-center justify-center rounded-xl border-2 bg-neutral-100 p-5 py-12 shadow-sm dark:border-neutral-900 dark:bg-black">
+          <p className="text-center text-xl font-semibold">
+            Uh oh! It seems you haven&apos;t bookmarked any posts yet...
+          </p>
+          <p className="text-center opacity-70 dark:opacity-50">
+            TIP: you can bookmark posts by clicking the bookmark icon on the
+            bottom right of a post
+          </p>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="w-full">
-      <h1>Bookmarks</h1>
-      <p>Bookmarks page content</p>
+      <InfiniteScrollContainer
+        onBottomReached={() => hasNextPage && !isFetching && fetchNextPage()}
+        className="space-y-5"
+      >
+        {posts.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
+        {isFetchingNextPage && (
+          <Loader2 className="mx-auto my-3 animate-spin" />
+        )}
+      </InfiniteScrollContainer>
     </div>
   );
 }
--- src/components/posts/PostActionButtons.tsx
+++ src/components/posts/PostActionButtons.tsx
@@ -1,12 +1,12 @@
 import { PostData } from "@/lib/types";
 import Bookmark from "./actionbuttons/Bookmark";
 
-export default function PostActionButtons({ post }: { post: PostData }) {
+export default function PostActionButtons({ post }: { post: PostData}) {
   return (
     <div className="flex items-center justify-between">
       <div></div>
       <div>
-        <Bookmark post={post} />
+        <Bookmark  post={post} />
       </div>
     </div>
   );
